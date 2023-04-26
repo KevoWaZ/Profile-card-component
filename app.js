947cb94ebdedd040931e3b9f74449c52709b055d
Kevin Gauthier <kevin.gauthier145@gmail.com>
2023-04-26 02:49:24 +0200

    Valeurs reste
    Les valeurs sont enregistrer dans le local storage de sorte que les nouvelles valeurs soient les même a la réouverture de la page

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
-let name = document.querySelector('.text h1')
-let age = document.querySelector('.text span')
+// Récupérer les éléments du DOM
+const name = document.querySelector('.text h1');
+const age = document.querySelector('.text span');
+const image = document.querySelector('#logo');
+const input = document.querySelector('input');
 
-name.addEventListener('click', () => {
-    let newName = prompt('entrez votre nom et prénom')
-    name.firstChild.textContent = newName
-    let newAge = prompt('Entrez votre age')
-    age.lastChild.textContent = newAge
-})
+window.onload = () => {
+  // Vérifier s'il y a des données stockées pour le nom, l'âge et l'image
+  const storedName = localStorage.getItem('name');
+  const storedAge = localStorage.getItem('age');
+  const storedImage = localStorage.getItem('image');
+  if (storedName && storedAge) {
+    name.firstChild.textContent = storedName;
+    age.lastChild.textContent = storedAge;
+  }
+  if (storedImage) {
+    image.src = storedImage;
+  }
 
+  // Ajouter un événement au clic sur le nom pour changer les valeurs et les stocker
+  name.addEventListener('click', () => {
+    const newName = prompt('Entrez votre nom et prénom');
+    const newAge = prompt('Entrez votre âge');
 
+    // Vérifier si le nom et l'âge ont été saisis avant de mettre à jour le contenu et de stocker les nouvelles valeurs
+    if (newName && newAge) {
+      name.firstChild.textContent = newName;
+      age.lastChild.textContent = newAge;
 
-const image = document.querySelector('#logo')
-const input = document.querySelector('input')
+      localStorage.setItem('name', newName);
+      localStorage.setItem('age', newAge);
+    }
+  });
 
-input.addEventListener('change', (e) => {
-    image.src = URL.createObjectURL(e.target.files[0])
-})
+  // Ajouter un événement pour changer l'image en fonction du fichier sélectionné par l'utilisateur et la stocker
+  input.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+      image.src = reader.result;
+      localStorage.setItem('image', reader.result);
+    });
+
+    reader.readAsDataURL(file);
+  });
+};
